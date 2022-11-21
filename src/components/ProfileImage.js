b6a1fd28af0960c1a611ba50a015b97e748d5958
Marten <marten@jalakas.ee>
2022-11-22 00:01:52 +0200

    added blur to images. made 2x2 grid of cards.

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -3,6 +3,13 @@ import profilePic from "../images/profile-pic.jpg";
 
 export default function ProfileImage() {
   return (
-    <img src={profilePic} alt="This is Marten" className="profile-pic"></img>
+    <div className="profile-img-wrapper">
+      <img src={profilePic} alt="This is Marten" className="profile-pic"></img>
+      <img
+        src="https://i.paste.pics/373f53b9cbabf18ff37a67a77f43eb9e.png"
+        alt="hover me"
+        className="hover-me"
+      ></img>
+    </div>
   );
 }
